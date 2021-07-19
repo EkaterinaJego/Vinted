bc391c1ef6e274dd51fd9e9020d6f5013805f0f8
Ekaterina Jégo <cat-velichko@yandex.ru>
2021-07-19 23:14:00 +0200

    fix: get homepage

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
-const cloudinary = require("cloudinary").v2; 
+const cloudinary = require("cloudinary").v2;
 require('dotenv').config();
 const cors = require('cors');
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  });
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 app.use(formidable());
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI, {
-useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true})
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
 
 const userRoutes = require('./routes/user');
 app.use(userRoutes);
@@ -24,16 +25,16 @@ app.use(userRoutes);
 const offerRoutes = require('./routes/offer');
 app.use(offerRoutes);
 
-app.get('https://my-vinted-backend-project.herokuapp.com/', (req, res) => {
-  res.json("Bienvenue sur la page API Vinted");p
+app.get('/', (req, res) => {
+  res.json("Bienvenue sur la page API Vinted")
 })
 
-app.all("*", (req,res) => {
-  res.status(404).json({error: "Page not found"})
+app.all("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" })
 })
 
 app.listen(process.env.PORT, () => {
-    console.log("Server has started")
+  console.log("Server has started")
 });
 
 

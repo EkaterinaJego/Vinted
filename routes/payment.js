69d3adc7c257166d11a969f308f6a2f398786346
Ekaterina Jégo <cat-velichko@yandex.ru>
2021-09-09 22:17:58 +0200

    finalization payment

--- routes/payment.js
+++ routes/payment.js
@@ -2,20 +2,18 @@ const express = require("express");
 const router = express.Router();
 const createStripe = require("stripe");
 
-/* Votre clé privée doit être indiquée ici */
-
+/* Clé privée : */
 const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
 
 // on réceptionne le token
-// router.post("/payment", async (req, res) => {
+
 router.post("/payment", async (req, res) => {
   try {
     const stripeToken = req.fields.token;
-    console.log("STRIPETOKEN==>", stripeToken);
-
+    console.log("AMOUNT==>", req.fields.amount);
     const response = await stripe.charges.create({
-      // amount: Number(req.fields.amount) * 100,
-      amount: 2000,
+      amount: req.fields.amount * 100,
+      // amount: 2000,
       currency: "eur",
       description: req.fields.title,
       source: stripeToken,
@@ -24,7 +22,7 @@ router.post("/payment", async (req, res) => {
     // TO DO : MàJ base de données
     // Réponse au client pour afficher un message de statut:
     res.status(200).json(response);
-    console.log(response.data);
+    console.log(response);
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ Error: error.response });

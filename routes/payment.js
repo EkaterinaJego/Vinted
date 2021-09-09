5060e71df81314c0ade33211df320018fbb7add3
Ekaterina Jégo <cat-velichko@yandex.ru>
2021-09-09 21:22:36 +0200

    fixing payment2

--- routes/payment.js
+++ routes/payment.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const createStripe = require("stripe");
-const formidableMiddleware = require("express-formidable");
-router.use(formidableMiddleware());
 
 /* Votre clé privée doit être indiquée ici */
 const stripe = createStripe(process.env.STRIPE_API_SECRET);
 
 // on réceptionne le token
+// router.post("/payment", async (req, res) => {
 router.post("/payment", async (req, res) => {
   try {
     const stripeToken = req.fields.token;
